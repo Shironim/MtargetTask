8d943d05c6c1c0079b758f83d857335406ea5d2f
shironim <dimasseto.id@gmail.com>
2022-10-21 08:57:12 +0700

    add font poppins

--- src/Component/ButtonTime.js
+++ src/Component/ButtonTime.js
@@ -1,7 +1,7 @@
 const ButtonTime = ({ disabled, tanda, addTime, minusTime }) => {
   return (
     <button disabled={disabled} onClick={tanda === "+" ? addTime : minusTime} className="rounded-circle d-flex justify-content-center align-self-center" style={{ width: "50px", height: "50px", marginRight: "16px", border: "1px solid white", boxShadow: "0px 2px 4px 1px rgba(74,74,74,0.3)", backgroundColor: disabled ? "red" : "white" }}>
-      <h2 className="text-dark">{tanda}</h2>
+      <h2 className="text-dark m-0 p-0 align-self-center">{tanda}</h2>
     </button>
   )
 }
--- src/Component/Timer.js
+++ src/Component/Timer.js
@@ -4,7 +4,7 @@ const Timer = ({ minutes, seconds, circle1, circle2, circle3, clock }) => {
       <div className="rounded-circle d-flex align-self-center justify-content-center" style={{ width: `${circle2}`, height: `${circle2}`, backgroundColor: "#d86151", boxShadow: "0px 0px 8px 2px rgba(74,74,74,0.4)" }}>
         <div className="rounded-circle d-flex align-self-center justify-content-center" style={{ border: "3px solid #000", width: `${circle1}`, height: `${circle1}`, borderColor: "#cdcdcdbd" }}>
           <div style={{ alignSelf: "center" }}>
-            <h1 className="p-4 text-light">
+            <h1 className="p-2 text-light">
               {`${clock(minutes)} : ${clock(seconds)}`}
             </h1>
           </div>
